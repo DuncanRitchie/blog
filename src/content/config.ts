e49c9e575e28eeb17972741ab4699a17ba1077be
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2023-01-29 21:50:41 +0000

    Set posts collection up

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { z, defineCollection } from 'astro:content';
+
+const postsCollection = defineCollection({
+  schema: z.object({
+    date: z.date(),
+    title: z.string(),
+    draft: z.boolean(),
+    tags: z.array(z.string()),
+  })
+});
+
+// Keys here should match the collection directory names in "src/content"
+export const collections = {
+  'posts': postsCollection,
+};
