4d90a1bcadf2764a75c17b4cc50bc38f1e38d58a
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2023-02-04 14:41:24 +0000

    Code-blocks look nicer

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,11 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	base: '/blog',
-	site: 'https://www.duncanritchie.co.uk/blog'
+	site: 'https://www.duncanritchie.co.uk/blog',
+	markdown: {
+		shikiConfig: {
+			// Available themes are in node_modules/shiki/themes/
+			theme: 'slack-dark',
+		},
+	},
 });
