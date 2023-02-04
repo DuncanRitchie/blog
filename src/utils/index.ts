2f2609416bb2c9e63a2940848ec81061f28a9ce3
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2023-02-04 18:52:19 +0000

    More comments on utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,17 @@
 import type { CollectionEntry } from 'astro:content'
 
-/** This function exists because `blogpost.slug` includes the
+/**
+ * Returns the slug (URL fragment) for the given blogpost.
+ * This function exists because `blogpost.slug` includes the
  * subfolder (eg “2020/”), but blogpost URLs should not include this.
  */
 const slugifyPost = (blogpost: CollectionEntry<'posts'>) => {
 	return blogpost.slug.replace(/.+\//, '')
 }
 
+/**
+ * Eg, "Annual review" => "annual-review"
+ */
 const slugifyText = (text: string) => {
 	return text
 		.trim()
@@ -14,6 +19,9 @@ const slugifyText = (text: string) => {
 		.toLowerCase()
 }
 
+/**
+ * Returns the given posts, sorted reverse-chronologically.
+ */
 const sortPosts = (posts: CollectionEntry<'posts'>[]) => {
 	return posts
 		.filter((post) => !post.data.draft)
