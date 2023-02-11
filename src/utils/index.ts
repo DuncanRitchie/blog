1a1085302deebd37abd0d0a1a8c0e82eb04024e8
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2023-02-11 17:10:50 +0000

    Logs if draft status is ambiguous

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,40 @@
-import type { CollectionEntry } from 'astro:content'
+import { CollectionEntry, getCollection } from 'astro:content'
+
+/**
+ * Draft posts should have `draft:true` in frontmatter and “.draft.md” ending the filename.
+ * This function logs if a post has one of those criteria but not the other.
+ */
+const isDraftStatusAmbiguous = (
+	blogpost: CollectionEntry<'posts'>,
+): boolean => {
+	const hasDraftFrontmatter = blogpost.data.draft
+	const isExcludedFromGit = blogpost.id.endsWith('.draft.md')
+	if (hasDraftFrontmatter === isExcludedFromGit) {
+		return false
+	}
+	if (hasDraftFrontmatter) {
+		console.warn(
+			'Marked as draft in frontmatter but not filename: ' + blogpost.id,
+		)
+	} else {
+		console.warn(
+			'Marked as draft in filename but not frontmatter: ' + blogpost.id,
+		)
+	}
+	return true
+}
+
+/**
+ * Logs to the console if any posts have ambiguous draft status.
+ * These posts will need to be reviewed manually and their status clarified.
+ * @returns any posts with ambiguous draft status
+ */
+const logIfPostsHaveAmbiguousDraftStatus = async (): Promise<
+	CollectionEntry<'posts'>[]
+> => {
+	const allPosts = await getCollection('posts')
+	return allPosts.filter(isDraftStatusAmbiguous)
+}
 
 /**
  * Returns the slug (URL fragment) for the given blogpost.
@@ -39,4 +75,9 @@ const sortPosts = (posts: CollectionEntry<'posts'>[]) => {
 	return posts.sort((a, b) => (a.data.date < b.data.date ? 1 : -1))
 }
 
-export { slugifyPost, slugifyText, sortPosts }
+export {
+	logIfPostsHaveAmbiguousDraftStatus,
+	slugifyPost,
+	slugifyText,
+	sortPosts,
+}
