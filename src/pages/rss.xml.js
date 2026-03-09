e7084a9027440864e7b0bd1b0b460635d93361b1
Duncan Ritchie <duncanritchie@btinternet.com>
2026-03-09 19:57:06 +0000

    RSS feed for all posts handles MDX and escapes HTML

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,7 @@
 import rss from '@astrojs/rss'
-import { getContainerRenderer as getMdxRenderer } from '@astrojs/mdx'
-import { experimental_AstroContainer as AstroContainer } from 'astro/container'
-import { loadRenderers } from 'astro:container'
-import { getCollection, render } from 'astro:content'
+import { getCollection } from 'astro:content'
 
-import { addLinkBase, escapeHtml, slugifyPost, sortPosts } from '../utils/'
+import { addLinkBase, rssItemsMdx, sortPosts } from '../utils/'
 
 // Code adapted for MDX from https://blog.damato.design/posts/astro-rss-mdx/
 
@@ -12,30 +9,12 @@ export async function GET(context) {
 	const posts = await getCollection('posts')
 	const postsSorted = sortPosts(posts)
 
-	const renderers = await loadRenderers([getMdxRenderer()])
-	const container = await AstroContainer.create({ renderers })
-
-	// Use Astro Containers to render the posts to HTML.
-	const items = []
-	for (const post of postsSorted) {
-		const { Content } = await render(post)
-		const content = await container.renderToString(Content)
-		const link = addLinkBase(slugifyPost(post))
-		items.push({
-			...post.data,
-			link,
-			content,
-			title: escapeHtml(post.data.title),
-			pubDate: new Date(post.data.date),
-		})
-	}
-
 	return rss({
 		title: 'Duncan Ritchie’s blog',
 		description:
 			'Assorted notes about software development and other matters that I find interesting.',
 		site: context.site,
-		items,
+		items: rssItemsMdx(postsSorted),
 		customData: `<language>en-gb</language>`,
 		stylesheet: addLinkBase('rss-styles.xsl'),
 	})
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,10 @@
-import { type CollectionEntry } from 'astro:content'
+import { render, type CollectionEntry } from 'astro:content'
+import { getContainerRenderer as getMdxRenderer } from '@astrojs/mdx'
+import { experimental_AstroContainer as AstroContainer } from 'astro/container'
+import { loadRenderers } from 'astro:container'
 import sanitizeHtml from 'sanitize-html'
 import MarkdownIt from 'markdown-it'
+
 const parser = new MarkdownIt({ html: true, breaks: true, linkify: true })
 
 /**
@@ -46,8 +50,14 @@ const absoluteLinksInHtml = (html: string, pagePath: string) => {
 	return newHtml
 }
 
+// I have two methods for generating the `items` field for RSS feeds.
+// One uses the MDX renderer (which should handle MDX and standard Markdown);
+// the other uses Astro’s default renderer (which treats MDX as standard Markdown).
+// Currently, the RSS feed with all posts is using `rssItemsMdx`,
+// and RSS feeds for specific tags are using `rssItems`, so I can test both functions.
+
 /**
- * Generats the `items` field for rss.xml.js files.
+ * Generates the `items` field for rss.xml.js files, WITHOUT using the MDX renderer.
  * @param posts Array of posts as collection entries
  * @returns The `items` field for the RSS feed
  */
@@ -61,6 +71,35 @@ const rssItems = (posts: CollectionEntry<'posts'>[]) => {
 			absoluteLinksInHtml(parser.render(post.body), slugifyPost(post)),
 		),
 	}))
+}
+
+/**
+ * Generates the `items` field for rss.xml.js files, USING the MDX renderer.
+ * @param posts Array of posts as collection entries
+ * @returns The `items` field for the RSS feed
+ */
+const rssItemsMdx = async (postsSorted: CollectionEntry<'posts'>[]) => {
+	const renderers = await loadRenderers([getMdxRenderer()])
+	const container = await AstroContainer.create({ renderers })
+	// Use Astro Containers to render the posts to HTML.
+	const items = []
+	for (const post of postsSorted) {
+		const { Content } = await render(post)
+		const content = sanitizeHtml(
+			absoluteLinksInHtml(
+				await container.renderToString(Content),
+				slugifyPost(post),
+			),
+		)
+		const link = addLinkBase(slugifyPost(post))
+		items.push({
+			...post.data,
+			link,
+			content,
+			title: escapeHtml(post.data.title),
+			pubDate: new Date(post.data.date),
+		})
+	}
 }
 
 /**
@@ -116,6 +155,7 @@ export {
 	addLinkBase,
 	escapeHtml,
 	rssItems,
+	rssItemsMdx,
 	slugifyPost,
 	slugifyText,
 	sortPosts,
